3e0cbc3271bb7c2d88cd06472a176d9460c35a9e
alexander <alexander@barpal.site>
2022-09-16 15:43:42 -0700

    new php testing

--- php/index.php
+++ php/index.php
@@ -6,11 +6,23 @@
         <?php
             echo "<p>Hello World!</p>";
         ?>
-        <div>
-        <a href="login.php"> Click here to login
-        </div>
-        <div>
-        <a href="register.php"> Click here to register 
-        </div>
     </body>
 </html> 
+<?php
+// Connect to the database server
+$mysql = mysqli_connect("localhost", "admin", "P4ss112701.") or die(mysqli_error($mysql));
+
+// Open to the database
+mysqli_select_db($mysql, "barpalsite_PHP_test") or die(mysqli_error($mysql));
+
+// Select all records from the "Individual" table
+$result = mysqli_query($mysql, "SELECT * FROM lifetimes")
+or die(mysqli_error($mysql));
+
+// Loop thru each record (using the PHP $row variable),
+// then display the first name and last name of each record.
+while($row = mysqli_fetch_array($result)){
+	echo $row['FirstName']. " - ". $row['LastName'];
+	echo "<br>";
+}
+?>
